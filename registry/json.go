697071a1a63112ec612342032af2c93849a19ab2
Michael Bridgen <mikeb@squaremobius.net>
2016-09-27 13:09:04 +0100

    Follow Link headers in tag/list and /_catalog responses

--- registry/json.go
+++ registry/json.go
@@ -2,6 +2,13 @@ package registry
 
 import (
 	"encoding/json"
+	"errors"
+	"net/http"
+	"regexp"
+)
+
+var (
+	ErrNoMorePages = errors.New("No more pages")
 )
 
 func (registry *Registry) getJson(url string, response interface{}) error {
@@ -20,4 +27,42 @@ func (registry *Registry) getJson(url string, response interface{}) error {
 	}
 
 	return nil
+}
+
+// getPaginatedJson accepts a string and a pointer, and returns the
+// next page URL while updating pointed-to variable with a parsed JSON
+// value. When there are no more pages it returns `ErrNoMorePages`.
+func (registry *Registry) getPaginatedJson(url string, response interface{}) (string, error) {
+	resp, err := registry.Client.Get(url)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return "", err
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(response)
+	if err != nil {
+		return "", err
+	}
+	return getNextLink(resp)
+}
+
+var linkRE *regexp.Regexp = regexp.MustCompile(`^ *<?([^;>]+)>?(?:; *([a-z]+)="?([^";]*)"?)*$`)
+
+func getNextLink(resp *http.Response) (string, error) {
+	for _, link := range resp.Header[http.CanonicalHeaderKey("Link")] {
+		parts := linkRE.FindStringSubmatch(link)
+		if len(parts) < 4 {
+			continue
+		}
+		// We have a structure like []string{(whole match), URL, key, value, ...}
+		for i := 2; i < len(parts); i += 2 {
+			if parts[i] == "rel" && parts[i+1] == "next" {
+				return parts[1], nil
+			}
+		}
+	}
+	return "", ErrNoMorePages
 }
--- registry/repositories.go
+++ registry/repositories.go
@@ -4,14 +4,22 @@ type repositoriesResponse struct {
 	Repositories []string `json:"repositories"`
 }
 
-func (registry *Registry) Repositories() ([]string, error) {
+func (registry *Registry) Repositories() (repos []string, err error) {
 	url := registry.url("/v2/_catalog")
-	registry.Logf("registry.repositories url=%s", url)
 
 	var response repositoriesResponse
-	if err := registry.getJson(url, &response); err != nil {
-		return nil, err
+	for {
+		registry.Logf("registry.repositories url=%s", url)
+		url, err = registry.getPaginatedJson(url, &response)
+		switch err {
+		case ErrNoMorePages:
+			repos = append(repos, response.Repositories...)
+			return repos, nil
+		case nil:
+			repos = append(repos, response.Repositories...)
+			continue
+		default:
+			return nil, err
+		}
 	}
-
-	return response.Repositories, nil
 }
