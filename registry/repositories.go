1d0f2e6e2b2fc76719485af209b33eddc01393a6
Josh Mahowald <joshua.mahowald@gmail.com>
2016-12-02 11:19:16 -0600

    Removing named return string

--- registry/repositories.go
+++ registry/repositories.go
@@ -4,9 +4,10 @@ type repositoriesResponse struct {
 	Repositories []string `json:"repositories"`
 }
 
-func (registry *Registry) Repositories() (repos []string, err error) {
+func (registry *Registry) Repositories() ([]string, error) {
 	url := registry.url("/v2/_catalog")
-
+	repos := make([]string, 0, 10)
+	var err error //We create this here, otherwise url will be rescoped with :=
 	var response repositoriesResponse
 	for {
 		registry.Logf("registry.repositories url=%s", url)
