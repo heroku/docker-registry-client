49f7f1916ffdefbd63de71b694b59bd73ccb2a86
Pavel Ageev <ageev.pavel.v@gmail.com>
2017-04-04 14:39:05 +0300

    Add correct header for fetch digest

--- registry/manifest.go
+++ registry/manifest.go
@@ -73,13 +73,17 @@ func (registry *Registry) ManifestDigest(repository, reference string) (digest.D
 	url := registry.url("/v2/%s/manifests/%s", repository, reference)
 	registry.Logf("registry.manifest.head url=%s repository=%s reference=%s", url, repository, reference)
 
-	resp, err := registry.Client.Head(url)
-	if resp != nil {
-		defer resp.Body.Close()
-	}
+	req, err := http.NewRequest("HEAD", url, nil)
 	if err != nil {
 		return "", err
 	}
+
+	req.Header.Set("Accept", manifestV2.MediaTypeManifest)
+	resp, err := registry.Client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 	return digest.ParseDigest(resp.Header.Get("Docker-Content-Digest"))
 }
 
