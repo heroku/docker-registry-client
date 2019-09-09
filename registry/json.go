c7b441c09351dc13d98048d8b4e530fa86c5990d
Edward Muller <emuller@salesforce.com>
2019-09-09 10:34:37 -0700

    var `uploadUrl` should be `uploadURL` (golint)

--- registry/json.go
+++ registry/json.go
@@ -11,7 +11,7 @@ var (
 	ErrNoMorePages = errors.New("No more pages")
 )
 
-func (registry *Registry) getJson(url string, response interface{}) error {
+func (registry *Registry) getJSON(url string, response interface{}) error {
 	resp, err := registry.Client.Get(url)
 	if err != nil {
 		return err
@@ -27,10 +27,10 @@ func (registry *Registry) getJson(url string, response interface{}) error {
 	return nil
 }
 
-// getPaginatedJson accepts a string and a pointer, and returns the
+// getPaginatedJSON accepts a string and a pointer, and returns the
 // next page URL while updating pointed-to variable with a parsed JSON
 // value. When there are no more pages it returns `ErrNoMorePages`.
-func (registry *Registry) getPaginatedJson(url string, response interface{}) (string, error) {
+func (registry *Registry) getPaginatedJSON(url string, response interface{}) (string, error) {
 	resp, err := registry.Client.Get(url)
 	if err != nil {
 		return "", err
